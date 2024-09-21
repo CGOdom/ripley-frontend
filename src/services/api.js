67f9039cae63398b7e418560cef40596246a9efd
CGOdom <cadengeneodom@gmail.com>
2024-09-20 22:11:20 -0500

    Hardcode api url

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 // Determine the base URL from environment variables or default to localhost
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+const BASE_URL = 'https://qq5t8z-3000.csb.app/' // process.env.REACT_APP_API_BASE_URL; // || 'http://localhost:3000';
 
 // Create an Axios instance with predefined configurations
 const api = axios.create({
